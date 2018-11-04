7a300d0faaab827d56726bb4938d1f4dd2de5d3d
johnnydrama92 <james.ferrier01@gmail.com>
2018-11-04 17:31:09 +0000

    comments

--- constants.go
+++ constants.go
@@ -3,17 +3,28 @@ package main
 import "fmt"
 import "math"
 
+// `const` declares a constant value.
 const s string = "constant"
 
 func main() {
-  fmt.Println(s)
+    fmt.Println(s)
 
-  const n = 50000000
+    // A `const` statement can appear anywhere a `var`
+    // statement can.
+    const n = 500000000
 
-  const d = 3e20 / n
-  fmt.Println(d)
+    // Constant expressions perform arithmetic with
+    // arbitrary precision.
+    const d = 3e20 / n
+    fmt.Println(d)
 
-  fmt.Println(int64(d))
+    // A numeric constant has no type until it's given
+    // one, such as by an explicit cast.
+    fmt.Println(int64(d))
 
-  fmt.Println(math.Sin(n))
+    // A number can be given a type by using it in a
+    // context that requires one, such as a variable
+    // assignment or function call. For example, here
+    // `math.Sin` expects a `float64`.
+    fmt.Println(math.Sin(n))
 }
